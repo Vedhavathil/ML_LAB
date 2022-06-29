1c38c27d7068c5e6dd5329c945cd6b74dbf9555b
Vedhavathil <82796185+Vedhavathil@users.noreply.github.com>
2022-06-29 10:39:26 +0530

    Update lab6.py

--- Lab-6:K-Means/lab6.py
+++ Lab-6:K-Means/lab6.py
@@ -1,1 +1,72 @@
+import numpy as np 
+import pandas as pd 
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.read_csv('Desktop/Mall_Customers.csv')
+df.head()
+df.describe()
+df.isnull().sum() # to check for missing data
+df.shape
+sns.countplot(df['Gender'])
+sns.distplot(df['Age'])
+sns.distplot(df['Annual Income (k$)'])
+sns.distplot(df['Spending Score (1-100)'])
+# Elbow method to find the optimal number of Clusters
+data=df.iloc[:,[3,4]].values
+from sklearn.cluster import KMeans
+wcss=[] # within cluster sum of square
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i, init='k-means++',random_state=0)
+    kmeans.fit(data)
+    wcss.append(kmeans.inertia_) #inertia_ = to find the wcss value
 
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+kmeans=KMeans(n_clusters=5,init='k-means++',random_state=0)
+y_kmeans=kmeans.fit_predict(data)
+#plotting the the clusters
+fig,ax = plt.subplots(figsize=(14,6))
+ax.scatter(data[y_kmeans==0,0],data[y_kmeans==0,1],s=100,c='red',label='Cluster 1')
+ax.scatter(data[y_kmeans==1,0],data[y_kmeans==1,1],s=100,c='blue',label='Cluster 2')
+ax.scatter(data[y_kmeans==2,0],data[y_kmeans==2,1],s=100,c='green',label='Cluster 3')
+ax.scatter(data[y_kmeans==3,0],data[y_kmeans==3,1],s=100,c='cyan',label='Cluster 4')
+ax.scatter(data[y_kmeans==4,0],data[y_kmeans==4,1],s=100,c='magenta',label='Cluster 5')
+
+ax.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=400,c='yellow',label='Centroid')
+plt.title('Cluster Segmentation of Customers')
+plt.xlabel('Annual Income(K$)')
+plt.ylabel('Spending Score(1-100)')
+plt.legend()
+plt.show()
+data = df.iloc[:,[2,4]].values
+from sklearn.cluster import KMeans
+wcss=[]  # within cluster sum of square
+for i in range(1,11):
+    kmeans=KMeans(n_clusters=i, init='k-means++',random_state=0)
+    kmeans.fit(data)
+    wcss.append(kmeans.inertia_)  # inertia_ = to find the wcss value
+
+plt.plot(range(1,11),wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+kmeans=KMeans(n_clusters=4,init='k-means++',random_state=0)
+y_kmeans=kmeans.fit_predict(data)
+
+#Plotting the clusters
+fig,ax = plt.subplots(figsize=(14,6))
+ax.scatter(data[y_kmeans==0,0],data[y_kmeans==0,1],s=100,c='red',label='Cluster 1')
+ax.scatter(data[y_kmeans==1,0],data[y_kmeans==1,1],s=100,c='blue',label='Cluster 2')
+ax.scatter(data[y_kmeans==2,0],data[y_kmeans==2,1],s=100,c='green',label='Cluster 3')
+ax.scatter(data[y_kmeans==3,0],data[y_kmeans==3,1],s=100,c='cyan',label='Cluster 4')
+
+ax.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=400,c='yellow',label='Centroid')
+plt.title('Cluster Segmentation of Customers')
+plt.xlabel('Age')
+plt.ylabel('Spending Score(1-100)')
+plt.legend()
+plt.show()
